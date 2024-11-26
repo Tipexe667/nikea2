c154800a4b731fe6c03b86260df65e67204ad6c9
Tipexe667 <tprayez@gmail.com>
2024-11-26 09:42:07 +0100

    Update env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,7 @@
 import zod from "zod";
 
 const envSchema = zod.object({
-  DATABASE_URL: zod.string().nonempty(),
+  DATABASE_URL = "mongodb+srv://hubaulouen:abT6TlQAItntJS5u@cluster0.md8ve.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0",
   GOOGLE_CLIENT_ID = "630039385758-uhb7jup2uvisem0v0uu3a903jgun36bn.apps.googleusercontent.com",
   GOOGLE_CLIENT_SECRET = "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",
   NEXTAUTH_URL="http://localhost:3000",

a61898a3833ffba7d1fc7630380b32d10209de3e
Tipexe667 <tprayez@gmail.com>
2024-11-26 10:41:23 +0100

    Update env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -12,7 +12,7 @@ const env = envSchema.parse({
   DATABASE_URL: "mongodb+srv://hubaulouen:abT6TlQAItntJS5u@cluster0.md8ve.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0",
   GOOGLE_CLIENT_ID: "630039385758-uhb7jup2uvisem0v0uu3a903jgun36bn.apps.googleusercontent.com",
   GOOGLE_CLIENT_SECRET: "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",
-  NEXTAUTH_SECRET: "kK1HZx+oN9w8b1iM3V5Afn9QK8Y2MlOQ4+AQWgLpGso=",
+  NEXTAUTH_SECRET: "fU2/V+vI4x2x26nZPgoShxvurIpN7PMFo3hNZkkdHCmBB3OckMs33AwoNX4=",
   NEXTAUTH_URL: "http://localhost:3000",
 });
 

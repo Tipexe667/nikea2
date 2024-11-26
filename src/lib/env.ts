37947cebfa32bb9a0eb611f4fe399d062a0042d5
Tipexe667 <tprayez@gmail.com>
2024-11-26 10:26:49 +0100

    Update env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,12 +4,16 @@ const envSchema = zod.object({
   DATABASE_URL: zod.string(),
   GOOGLE_CLIENT_ID: zod.string(),
   GOOGLE_CLIENT_SECRET: zod.string(),
+  NEXTAUTH_SECRET: zod.string(),
+  NEXTAUTH_URL: zod.string(),
 });
 
 const env = envSchema.parse({
   DATABASE_URL: "mongodb+srv://hubaulouen:abT6TlQAItntJS5u@cluster0.md8ve.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0",
   GOOGLE_CLIENT_ID: "630039385758-uhb7jup2uvisem0v0uu3a903jgun36bn.apps.googleusercontent.com",
   GOOGLE_CLIENT_SECRET: "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",
+  NEXTAUTH_SECRET: "kK1HZx+oN9w8b1iM3V5Afn9QK8Y2MlOQ4+AQWgLpGso=",
+  NEXTAUTH_URL: "http://localhost:3000",
 });
 
 export default env;

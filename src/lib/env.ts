52a633c1eb1a7ce2be0c555a34e4e6dd9b78885c
Tipexe667 <tprayez@gmail.com>
2024-11-26 09:48:49 +0100

    Update env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,11 +1,15 @@
 import zod from "zod";
 
 const envSchema = zod.object({
-  DATABASE_URL = "mongodb+srv://hubaulouen:abT6TlQAItntJS5u@cluster0.md8ve.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0",
-  GOOGLE_CLIENT_ID = "630039385758-uhb7jup2uvisem0v0uu3a903jgun36bn.apps.googleusercontent.com",
-  GOOGLE_CLIENT_SECRET = "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",
-  NEXTAUTH_URL="http://localhost:3000",
-  NEXTAUTH_SECRET="kK1HZx+oN9w8b1iM3V5Afn9QK8Y2MlOQ4+AQWgLpGso=",
+  DATABASE_URL: zod.string(),
+  GOOGLE_CLIENT_ID: zod.string(),
+  GOOGLE_CLIENT_SECRET: zod.string(),
 });
 
-export const env = envSchema.parse(process.env);
+const env = envSchema.parse({
+  DATABASE_URL: "mongodb+srv://hubaulouen:abT6TlQAItntJS5u@cluster0.md8ve.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0",
+  GOOGLE_CLIENT_ID: "630039385758-uhb7jup2uvisem0v0uu3a903jgun36bn.apps.googleusercontent.com",
+  GOOGLE_CLIENT_SECRET: "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",
+});
+
+export default env;
